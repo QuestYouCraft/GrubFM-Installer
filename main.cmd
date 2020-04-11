8fd192cca33e54787af3ecac3c75fde878259fe5
QuestYouCraft <46100878+QuestYouCraft@users.noreply.github.com>
2020-04-11 17:26:47 +0300

    Update and rename install.cmd to main.cmd

--- main.cmd
+++ main.cmd
@@ -1,39 +1,3 @@
-::[Bat To Exe Converter]
-::
-::fBE1pAF6MU+EWHreyHcjLQlHcAWGMWK0OpEZ++Pv4Pq79VccGucnfe8=
-::fBE1pAF6MU+EWHreyHcjLQlHcAWGMWK0OpEZ++Pv4Pq7pV8IVusxa5uV36yLQA==
-::fBE1pAF6MU+EWHreyHcjLQlHcAWGMWK0OpEZ++Pv4Pq7pV8IVuQyNY3S1PqUKfBz
-::YAwzoRdxOk+EWAjk
-::fBw5plQjdCyDJGyX8VAjFBpQQQ2MAE+/Fb4I5/jH4+WArXIaRvc9foKV07eBQA==
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF65
-::cxAkpRVqdFKZSDk=
-::cBs/ulQjdF65
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpSI=
-::egkzugNsPRvcWATEpSI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+IeA==
-::cxY6rQJ7JhzQF1fEqQJhZkoaHErSXA==
-::ZQ05rAF9IBncCkqN+0xwdVsFAlXMbCXqZg==
-::ZQ05rAF9IAHYFVzEqQIXOg5WainCFWWpErQQ5O3pjw==
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDCOQKWmcK/U15vvv7uaLp199
-::cRolqwZ3JBvQF1fEqQIXOg5WainCFWWpErQQ5O3pjw==
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATExVc1Ow9tQxGhLmq8EtU=
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRmj5lcyDjYUZQqRKGq2CrAOiA==
-::Zh4grVQjdCyDJGyX8VAjFBpQQQ2MAE+/Fb4I5/jHyPiSoGswa8sxa5va1riLMq4W8kCE
-::YB416Ek+ZW8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 @echo off
 title GrubFM Installer
 echo GrubFM Installer by QuestYouCraft
